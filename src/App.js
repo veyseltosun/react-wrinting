fe1e686b43d77262620be2eb303226aa57b23a72
veyseltosun <postalarim1923@gmail.com>
2023-02-02 00:47:28 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React from 'react'
 
 function App() {
+
+  const time = () => new Date().toLocaleTimeString();
+  React.useEffect(() => {
+   setInterval(time, 1000)
+  }, [])
+  
+  setInterval( time, 1000);
+  
   return (
     <div className='container'>
 
       <header>
         <h3 className='head'>
-          
+
           This is a react coding practising Iplimantation!
         </h3>
       </header>
+      <h2>It is {time()}  ...</h2>
 
-      Hello World!
+
+
 
 
 
     </div>
   )
 }
+
 
 export default App
