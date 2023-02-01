a23fc157e115b2b9661946cfbe5a686b8f0b32e2
veyseltosun <postalarim1923@gmail.com>
2023-02-02 01:00:46 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function App() {
 
-  const time = () => new Date().toLocaleTimeString();
+  const timeGetHandler = () => new Date().toLocaleTimeString();
   React.useEffect(() => {
-   setInterval(time, 1000)
+   setInterval(timeGetHandler, 1000)
   }, [])
   
-  setInterval( time, 1000);
+  
   
   return (
     <div className='container'>
@@ -18,7 +18,7 @@ function App() {
           This is a react coding practising Iplimantation!
         </h3>
       </header>
-      <h2>It is {time()}  ...</h2>
+      <h2>It is {timeGetHandler}  ...</h2>
 
 
 
